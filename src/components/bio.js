b37689def59d2120d27c23c70c9d841323396d04
13imi <yuma.imi13@gmail.com>
2019-05-17 15:47:29 +0900

    Fix some config

--- src/components/bio.js
+++ src/components/bio.js
@@ -38,11 +38,10 @@ function Bio() {
               }}
             />
             <p>
-              Written by <strong>{author}</strong> who lives and works in San
-              Francisco building useful things.
+              <strong>イミー</strong> 
               {` `}
               <a href={`https://twitter.com/${social.twitter}`}>
-                You should follow him on Twitter
+              ＠{author}
               </a>
             </p>
           </div>
@@ -54,7 +53,7 @@ function Bio() {
 
 const bioQuery = graphql`
   query BioQuery {
-    avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+    avatar: file(absolutePath: { regex: "/avatar.jpg/" }) {
       childImageSharp {
         fixed(width: 50, height: 50) {
           ...GatsbyImageSharpFixed
